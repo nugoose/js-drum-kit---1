775227c6994ca11e5d6831e4525bec7b672d7d37
nugoose <nugoose@gmail.com>
2017-01-06 23:21:41 -0500

    finalized js, project is done

--- drum_kit.js
+++ drum_kit.js
@@ -15,70 +15,118 @@ domReady(function() {
         tink = new Audio('sounds/tink.wav');
     document.onkeydown = getKey;
     
-    //clicks
+    //////////////clicks/////////////////
     //A
     document.getElementById('key_a').addEventListener('click', function(e){
         clap.play();
         e = document.getElementById('key_a');
-        e.classList.add('music');
+        animateMe(e);
         clap.currentTime = 0;
     }, false);
     //S
     document.getElementById('key_s').addEventListener('click', function(e){
         hihat.play();
+        e = document.getElementById('key_s');
+        animateMe(e);
+        hihat.currentTime = 0;
     }, false);
     //D
     document.getElementById('key_d').addEventListener('click', function(e){
         kick.play();
+        e = document.getElementById('key_d');
+        animateMe(e);
+        kick.currentTime = 0;
     }, false);
     //F
     document.getElementById('key_f').addEventListener('click', function(e){
         openhat.play();
+        e = document.getElementById('key_f');
+        animateMe(e);
+        openhat.currentTime = 0;
     }, false);
     //G
     document.getElementById('key_g').addEventListener('click', function(e){
         boom.play();
+        e = document.getElementById('key_g');
+        animateMe(e);
+        boom.currentTime = 0;
     }, false);
     //H
     document.getElementById('key_h').addEventListener('click', function(e){
         ride.play();
+        e = document.getElementById('key_h');
+        animateMe(e);
+        ride.currentTime = 0;
     }, false);
     //J
     document.getElementById('key_j').addEventListener('click', function(e){
         snare.play();
+        e = document.getElementById('key_j');
+        animateMe(e);
+        snare.currentTime = 0;
     }, false);
     //K
     document.getElementById('key_k').addEventListener('click', function(e){
         tom.play();
+        e = document.getElementById('key_k');
+        animateMe(e);
+        tom.currentTime = 0;
     }, false);
     //L
     document.getElementById('key_l').addEventListener('click', function(e){
         tink.play();
+        e = document.getElementById('key_l');
+        animateMe(e);
+        tink.currentTime = 0;
     }, false);
     
-    //keypresses
+    //////////////keypresses///////////////
     function getKey(e){
         var the_key = e.keyCode, el;
         if(the_key == 65){
             el = document.getElementById('key_a');
             el.click();
         }
-        else if (the_key == 83)
-            hihat.play();
-        else if (the_key == 68)
-            kick.play();
-        else if (the_key == 70)
-            openhat.play();
-        else if(the_key == 71)
-            boom.play();
-        else if(the_key == 72)
-            ride.play();
-        else if (the_key == 74)
-            snare.play();
-        else if (the_key == 75)
-            tom.play();
-        else if (the_key == 76)
-            tink.play();
+        else if (the_key == 83){
+            el = document.getElementById('key_s');
+            el.click();
+        }
+        else if (the_key == 68){
+            el = document.getElementById('key_d');
+            el.click();
+        }
+        else if (the_key == 70){
+            el = document.getElementById('key_f');
+            el.click();
+        }
+        else if(the_key == 71){
+            el = document.getElementById('key_g');
+            el.click();
+        }
+        else if(the_key == 72){
+            el = document.getElementById('key_h');
+            el.click();
+        }
+        else if (the_key == 74){
+            el = document.getElementById('key_j');
+            el.click();
+        }
+        else if (the_key == 75){
+            el = document.getElementById('key_k');
+            el.click();
+        }
+        else if (the_key == 76){
+            el = document.getElementById('key_l');
+            el.click();
+        }
+    }
+    
+    //////animation function/////
+    function animateMe(e){
+        e.classList.add('music');
+        setTimeout(function(){
+            e.classList.remove('music');
+        }, 80);
     }
     
 });
